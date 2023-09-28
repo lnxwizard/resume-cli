7fdfc54e0dd5e401a48c7d59d6607a5db89a7c97
lnxwizard <alperakca79@outlook.com>
2023-09-28 21:31:46 +0300

    Added GitHub workflow for Rust

--- src/about.rs
+++ src/about.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, World!");
+use colored::Colorize;
+
+pub fn print_about() {
+    println!("\n\nI'm Software Developer, Pixel Artist and Linux Enthusiast.");
+    println!("- I’m currently working on some CLI tool projects.");
+    println!("- I’m currently learning Go Programming Language and Rust.");
+    println!(
+        "- All of my projects are available at {}.",
+        "https://lnxwizard.github.io/projects".green()
+    );
+    println!(
+        "- I regularly write articles on {}.",
+        "https://lnxwizard.github.io/blog".green()
+    );
+    println!("- I'm From Turkey.");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+mod about;
+
+use about::print_about;
+use colored::Colorize;
+use inquire::Select;
+use std::process::exit;
+
+const NAME: &str = "Alper Akça";
+const GH_PROFILE: &str = "https://github.com/lnxwizard";
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "\n\nHi! I'm {}, a Rust and Go developer.",
+        NAME.bold().green()
+    );
+
+    let options = vec!["About", "Skills", "Tools", "Contact", "Exit"];
+
+    'main: loop {
+        let choice = Select::new("What would you like to know about me?", options.clone()).prompt();
+
+        match choice {
+            Ok(choice) => {
+                if choice == options[0] {
+                    print_about();
+                } else if choice == options[1] {
+                    println!("Skills");
+                } else if choice == options[2] {
+                    println!("Tools");
+                } else if choice == options[3] {
+                    println!("Contact");
+                } else if choice == options[4] {
+                    println!("Bye Bye...");
+                    exit(0);
+                }
+            }
+            Err(_) => println!(
+                "{:?} is not a valid option, please select a valid option.",
+                choice
+            ),
+        }
+    }
 }
