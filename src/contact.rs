8e5ae1479726ad97ef0d2646bd7c1b40a7fc70b5
lnxwizard <alperakca79@outlook.com>
2023-09-30 21:41:55 +0300

    update contacts

--- src/contact.rs
+++ src/contact.rs
@@ -1,8 +1,28 @@
 use colored::Colorize;
+use toml::Table;
 
-pub fn print_contact() {
+pub fn print_contact(data: &str) {
+    let value: Table = data.parse::<Table>().expect("Couldn't read data.");
+
     println!("\n\nYou can contact me at:");
-    println!("E-Mail: {}", "alperakca79@outlook.com".green());
-    println!("Reddit: {}", "u/AlperAkca79".green());
-    println!("Medium: {}\n", "medium.com/@lnxwizard".green());
+
+    // print e-mail
+    println!(
+        "{}: {}",
+        "E-Mail".bold(),
+        value["email"].as_str().unwrap().green()
+    );
+
+    // print reddit
+    println!(
+        "{}: {}",
+        "Reddit".bold(),
+        value["reddit"].as_str().unwrap().green()
+    );
+
+    println!(
+        "{}: {}",
+        "Medium".bold(),
+        value["medium"].as_str().unwrap().green()
+    );
 }
