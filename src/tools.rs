d793bb33e7e6a6e2c735e44bb76bb40080feb043
lnxwizard <alperakca79@outlook.com>
2023-09-30 21:42:06 +0300

    added tools

---
+++ src/tools.rs
@@ -0,0 +1,30 @@
+use colored::Colorize;
+use toml::Table;
+
+pub fn print_tools(data: &str) {
+    let value: Table = data.parse::<Table>().unwrap();
+
+    // print ide's
+    println!(
+        "\n\n{}: {}",
+        "IDE's".bold(),
+        value["ide"].as_str().unwrap().bright_green()
+    );
+
+    // print text editors
+    println!(
+        "{}: {}",
+        "Text Editors".bold(),
+        value["text_editor"].as_str().unwrap().bright_green()
+    );
+
+    // print version control systems
+    println!(
+        "{}: {}\n",
+        "Version Control System".bold(),
+        value["version_control_system"]
+            .as_str()
+            .unwrap()
+            .bright_green()
+    );
+}
